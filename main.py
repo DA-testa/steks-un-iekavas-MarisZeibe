34cdfa6948252bea7259dc72c241b466128cf99a
MarisZeibe <maris_zeibe@hotmail.com>
2023-02-19 17:34:04 +0200

    Stack brackets

--- main.py
+++ main.py
@@ -14,17 +14,23 @@ def find_mismatch(text):
     for i, next in enumerate(text):
         if next in "([{":
             # Process opening bracket, write your code here
+            opening_brackets_stack.append(Bracket(next, i))
             pass
 
         if next in ")]}":
             # Process closing bracket, write your code here
+            if len(opening_brackets_stack) == 0 or not are_matching(opening_brackets_stack.pop().char, next):
+                return i+1
             pass
+
+    return "Success" if len(opening_brackets_stack) == 0 else opening_brackets_stack.pop(0).position+1
 
 
 def main():
     text = input()
     mismatch = find_mismatch(text)
     # Printing answer, write your code here
+    print(mismatch)
 
 
 if __name__ == "__main__":
