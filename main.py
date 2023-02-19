5657750b000519746c29699843e3f46b467a7e42
MarisZeibe <maris_zeibe@hotmail.com>
2023-02-19 18:03:44 +0200

    Autograding test

--- main.py
+++ main.py
@@ -27,16 +27,10 @@ def find_mismatch(text):
 
 
 def main():
-    # Printing answer, write your code here
-    inputType = input()
     text = input()
-    if inputType == "F":
-        mismatch = find_mismatch(open("test/" + text, "r").read())
-    elif inputType == "I":
-        mismatch = find_mismatch(text)
-    else:
-        return
-    print(mismatch)
+    mismatch = find_mismatch(text)
+    # Printing answer, write your code here
+    print(text)
 
 
 if __name__ == "__main__":
